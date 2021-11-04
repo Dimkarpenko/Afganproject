adccd8d99b346b6b15d1f3ce734b7db26098971b
Dmitry Karpenko <dkarp3ncko@yandex.ru>
2021-11-04 18:41:28 +0500

    Create 503.php

---
+++ media/errors/503.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+    <title>OnCode - 503</title>
+    <link rel="icon" type="image/png" sizes="32x32" href="https://afganproject.tk/media/favicon.png">
+</head>
+<body>
+    <center>
+    <img src="http://oncodechannel.000webhostapp.com/media/errors/images/401.png" style="margin-top:15%;" height="50px">
+    <h1 style="text-shadow:#fff 0 0 20px;">503 Service Unavaiable</h1>
+    <h2>На сервере ведутся технические работы</h2>
+    </center>
+</div>
+<style>
+    :root {
+        color-scheme:dark;
+    }
+    
+    * {
+        font-family: 'Helvetica Neue', 'Segoe UI', 'Arial', sans-serif;
+    }
+</style>
