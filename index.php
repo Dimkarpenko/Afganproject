8144e79e804502389ffab22ba579fbc2e355e264
Dmitry Karpenko <dkarp3ncko@yandex.ru>
2021-11-04 18:26:09 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,296 @@
+<!DOCTYPE html>
+<head>
+    <meta name="yandex-verification" content="f9c8e31e1ba7f52e" />
+	<meta charset="utf-8">
+	<title>Нашим Афганцам</title>
+	<link rel="stylesheet" href="media/main.css">
+    <link rel='shortcut icon' href='media/favicon.png'>
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+    <meta name="description" content='Макет "Нашим Афганцам" - это уменьшенная обновлённого парка "Героям войны в Афганистане"'>
+    <meta name="keywords" content="afganistan,project,afganproject,layout">
+    <meta name="author" content="Dimkarpenko">
+    <meta name="copyright" content="Нашим Афганцам">
+    <meta property="og:locale" content="ru_RU" />
+    <meta property="og:url" content="http://afganproject.tk" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Нашим Афганцам" />
+    <meta property="og:description" content='Макет "Нашим Афганцам" - это уменьшенная обновлённого парка "Героям войны в Афганистане"' />
+    <meta property="og:image" content="http://afganproject.tk/media/preolader-2.png" />
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="@afganproject" />
+    <meta name="twitter:title" content="Нашим Афганцам" />
+    <meta name="twitter:image" content="http://afganproject.tk/media/preloader-2.png" />
+    <meta name="twitter:description" content='Макет "Нашим Афганцам" - это уменьшенная обновлённого парка "Героям войны в Афганистане"' />
+    <script type="module" src="lib/model-viewer.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-5DFVR2KP4L"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+    
+      gtag('config', 'G-5DFVR2KP4L');
+    </script>
+</head>
+<body>
+    <img src="media/preloader-2.png" style="visibility:hidden;display:none;">
+    <center>
+        <model-viewer src="media/source/t55.glb" alt="A 3D model of a T-55" environment-image="neutral" auto-rotate camera-controls style='width:90%;height:400px;--poster-color: transparent;background-color:transparent;margin-top:5%;' camera-orbit="145deg 0deg 0deg" max-camera-orbit="auto 90deg auto" min-camera-orbit="auto 90deg 51.63m" shadow-intensity="4" min-field-of-view="20deg" max-field-of-view="20deg" disable-zoom poster="media/preloader-1.png"><div class="progress-bar hide" slot="progress-bar"><div class="update-bar"></div></div></model-viewer>
+        <!--<button id="show"><img src="/media/fullscreen.png" width="25px" style="filter: invert(1);z-index:1;" alt="View in full screen"></button>-->
+        <div id="win">
+            <model-viewer src="media/source/t55.glb" alt="A 3D model of a T-55" ar ar-modes="webxr scene-viewer quick-look" environment-image="neutral" auto-rotate camera-controls style="width:100%;height:100%;--poster-color: transparent;--background-color:#1C2125;" max-field-of-view="30deg" min-field-of-view="20deg" camera-orbit="145deg 85deg 0deg" poster="media/preloader-1.png"><button id="hide">&#10006;</button><div class="progress-bar hide" slot="progress-bar"><div class="update-bar"></div></div></model-viewer>
+        </div>
+        <div id="question_win"><button id="question_hide">&#10006;</button><br><h2 style="text-shadow:#fff 0 0 20px;">Задать вопрос</h2><?php include 'lib/form/form.php';?></div>
+        <h1 style="text-shadow:#fff 0 0 20px;font-size:45px;">Нашим Афганцам</h1>
+        <h2 id="main-text" style="color:#fff;width:70%;"><i>Макет "Нашим Афганцам" - это уменьшенная копия обновлённого парка "Героям войны в Афганистане".Мы сделали перепланировку части этого парка, улучшили благоустройство и сам монумент. На нашем сайте представлено поэтапное создание этой работы,используемые материалы и видео по сборке модели танка.</i></h2>
+    	<h1 style='margin-top: 15%;text-shadow: #fff 0 0 20px;' id='video'>Как мы создавали проект</h1>
+    	<video src='media/video.mp4' controls style='width:70%'></video>
+    	</div>
+        <div id='about'>
+            <h1 style="text-shadow:#fff 0 0 20px;margin-top:10%">О пректе</h1>
+            <h2 style="color:#fff;width:70%"><i>Памятник воинам-афганцам находится на улице Республики у дома № 152 города Тюмени. Монумент расположен у здания Тюменского Колледжа Искусств. Торжественное открытие памятника состоялось 2 ноября 2002 года на площади имени Губкина, на месте, где 15 февраля 1999 года был установлен памятный гранитный камень в честь тюменцев, погибших в Афганистане при исполнении воинского долга.<br><br>
+                Наша работа представляет собой макет модернизации монумента “Памяти павшим в войне в
+                Афганистане” и благоустройство территории вокруг него. В настоящий момент в Тюмени уже есть 
+                памятник “Воинам - афганцам” на пересечении улиц Геологоразведчиков и Республики. Мы
+                решили его немного изменить, оставив старый монумент, и добавив к нему новый, более
+                интересный. Наш макет выполнен в масштабе 1/72, мы посчитали, что это будет наиболее
+                оптимальный размер. В период 1979 – 1990 г.г. в Афганистане проводились боевые действия с
+                применением различной техники. Помимо нового монумента мы решили добавить постамент
+                с танком Т-55, представленном выше, который принимал непосредственное участие в этом конфликте.На макете этот
+                танк представлен моделью. Какой же парк без деревьев? Деревья на нашей диораме сделаны
+                из сухих растений (сорняков и д.р.), а крона из измельченного поролона. Сам монумент, как и
+                основание сделаны из пеноплекса и полистерола. Раз есть деревья, значит должен быть и
+                газон, он сделан из пучков флока, которые установлены на специальное основание.</i></h2>
+        </div>
+        <a href="https://earth.google.com/web/@57.13486999,65.57209333,86.69669537a,0d,90y,251.28028626h,75.94726707t,0r/data=IjAKLEFGMVFpcFBkMVhpMnJkSzlIR3FEZHAtUWI3bWN6WlhNZFR0ZEhXalpzcnd6EAU" target='_blank' style="text-decoration:none;"><div class="model-btn-button"><div class="model-tx1-button"><span>Открыть в Google Earth</span></div></div></a><br>
+        <hr><br>
+        <iframe style='width:70%' height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=65.56599046174686%2C57.13281339460955%2C65.57786728326481%2C57.13716252397308&amp;layer=mapnik&amp;marker=57.13498802316427%2C65.57192887250585" style="border: 1px solid black"></iframe>
+        <h3 style="max-width:70%;">Местоположение монумента “Памяти павшим в войне в Афганистане”</h3>
+	</center>
+<div class=header style="z-index:3;">
+    <h3 align="center">
+        <a href='#' class='top-menu'>Главная</a>
+        <a href='#video' class='top-menu'>Видео</a>
+        <a href='#about' class='top-menu'>О проекте</a>
+        <a href='#footer' class='top-menu'>Контакты</a>
+        <a id="question" class='top-menu'>Задать вопрос</a>
+        <a href='https://github.com/dimkarpenko/afganproject' class='top-menu' target='_blank'>GitHub</a>
+    </h3>
+</div>
+<style type="text/css">#hellopreloader>p{display:none;}#hellopreloader_preload{display: block;position: fixed;z-index: 99999;top: 0;left: 0;width: 100%;height: 100%;min-width: 1000px;background: #14171A url(/media/preloader.svg) center center no-repeat;background-size:41px;}</style>
+<div id="hellopreloader"><div id="hellopreloader_preload"></div></div>
+<script type="text/javascript">var hellopreloader = document.getElementById("hellopreloader_preload");function fadeOutnojquery(el){el.style.opacity = 1;var interhellopreloader = setInterval(function(){el.style.opacity = el.style.opacity - 0.05;if (el.style.opacity <=0.05){ clearInterval(interhellopreloader);hellopreloader.style.display = "none";}},16);}window.onload = function(){setTimeout(function(){fadeOutnojquery(hellopreloader);    var $recaptcha = document.querySelector('#g-recaptcha-response');if($recaptcha) {$recaptcha.setAttribute("required", "");}},1000);};</script>
+</body>
+<div id="footer" width="100%"><br><br>
+    <table width="100%">
+    <tr>
+        <td style="text-align:center;">
+        <table width="800px" style="margin-left:5%;">
+            <tr>
+                <th style="text-align:center;font-size:25px;">Главная<br><br></th>
+                <th style="text-align:center;font-size:25px;">Авторы<br><br></th>
+                <th style="text-align:center;font-size:25px;">GitHub<br><br></th>
+                <th style="text-align:center;font-size:25px;">Контакты<br><br></th>
+            </tr>
+            <tr>
+                <td><a href="#" class="footer">Главная</a></td>
+                <td><a href="https://vk.com/dim.karpenko" target="_blank" class="footer">Дмитрий Карпенко</a></td>
+                <td><a href="https://github.com/dimkarpenko/afganproject" target="_blank" class="footer">Репозиторий</a></td>
+                <td><a href="mailto:d.karpenko@afganproject.tk" target="_blank" target="_blank" class="footer">d.karpenko@afganproject.tk</a></td>
+            </tr>
+            <tr>
+                <td><a href="#video" class="footer">Видео</a></td>
+                <td><a href="https://vk.com/npocto_lllypuk" target="_blank" class="footer">Александр Коломоец</a></td>
+                <td><a href="https://github.com/dimkarpenko/afganproject/issues" target="_blank" class="footer">Вопросы</a></td>
+                <td><a href="mailto:a.kolomoec@afganproject.tk" target="_blank" target="_blank" class="footer">a.kolomoec@afganproject.tk</a></td>
+            </tr>
+            <tr>
+                <td><a href="#about" class="footer">О проекте</a></td>
+                <td></td>
+                <td><a href="https://github.com/dimkarpenko/afganproject/graphs/contributors" target="_blank" class="footer">Участники</a></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td><a href="https://github.com/dimkarpenko/afganproject/commit/main" target="_blank" class="footer">Обновления</a></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td><a href="https://github.com/dimkarpenko/afganproject/projects" target="_blank" class="footer">Проекты</a></td>
+            </tr>
+            </table></td>
+            <td>
+                        <td><div class="footer-tx1-developer">
+        <h3>Сайт разработан <a style="color:#fff;" href="https://github.com/dimkarpenko" target="_blank">DIMKARPENKO</a></h3>
+        <h3>Текст писал <a style="color:#fff;" href="https://vk.com/npocto_lllypuk" target="_blank" target="_blank">Александр Коломоец</a></h3>
+        <h3>© 2021   <a href="http://<?php echo $_SERVER['HTTP_HOST'] ?>" style="color:#fff;"><?php echo $_SERVER['HTTP_HOST'] ?></a>  Тюмень</h3>
+        </div></td>
+            </td>
+</table><br><br><br>
+</div>
+<script>
+    let content = document.getElementById("win")
+    let show = document.getElementById("show")
+    let hide = document.getElementById("hide")
+    content.style.display = "none"
+        
+    show.addEventListener("click", () => {
+        content.style.display = "block"
+    })
+        
+    hide.addEventListener("click", () => {
+        content.style.display = "none"
+    })
+    $(document).ready(function () {
+        if (screen.width < 650) {
+            $("#show").hide();
+        }
+        else {
+    
+            $("#show").show();
+        }
+        
+        if (screen.width < 840) {
+            $(".footer-tx1-developer").hide();
+        }
+        else {
+    
+            $(".footer-tx1-developer").show();
+        }
+    
+    });
+</script>
+<script>
+$(document).ready(function(){
+  $("#question_win").hide();
+  $("#question_hide").click(function(){
+    $("#question_win").hide();
+  });
+  $("#question").click(function(){
+    $("#question_win").show();
+  });
+});
+</script>
+<style>
+
+    .mark_button {
+        background-color:#3D3D3D;
+        border:2px solid #494949;
+        width:120px;
+        height:30px;
+        cursor:pointer;
+        border-radius:3px;
+    }
+    
+    .mark_button:hover {
+        background-color:#464646;
+        border:2px solid #575757;
+    }
+
+    .model-tx1-button {
+        font-family: Roboto-Regular;
+        font-size: 14px;
+        line-height: 40px;
+        height: 100%;
+        text-transform: uppercase;
+        color: #fff;
+        font-weight: 700;
+    }
+    
+    .model-btn-button {
+        width: 230px;
+        height: 40px;
+        border-radius: 12px;
+        margin-top: 35px;
+        background: linear-gradient(
+    -135deg
+    ,rgb(109,186,166) 25%,rgb(151,186,109) 100%);
+        box-shadow: rgb(109 186 166 / 40%) 0 10px 32px 0;
+        cursor: pointer;
+        padding-left: 10px;
+        padding-right: 10px;
+    }
+    
+    .model-btn-button:hover {
+        background:linear-gradient(-135deg,rgb(131,218,196) 25%,rgb(180,220,132) 100%);
+    }
+
+    #question_win {
+        overflow-y: scroll;
+        border:5px solid #0F1214;
+        position:fixed;
+        background-color:#14171A;
+        z-index:5;
+        top:15%;
+        left:15%;
+        right:15%;
+        box-shadow: rgba(0,0,0,.5) 0 0 0 1000px;
+        bottom:15%; 
+    }
+    
+    #question_hide {
+        float:right;
+        background-color:transparent;
+        border:none;
+        color:#fff;
+        width:5%;
+        height:5%;
+    }
+    
+    #question_hide:hover{
+        cursor:pointer;
+        background-color:#FF0000;
+    }
+
+    hr {
+        width:70%;
+        background-color:#fff;
+        box-shadow:#fff 0 0 20px;
+    }
+    
+    img[src*="https://cdn.000webhost.com/000webhost/logo/footer-powered-by-000webhost-white2.png"] {
+        display: none;
+    } 
+    
+    /*#show {
+        border:none;
+        height:40px;
+        margin-right:-800px;
+        background-color:#14171A;
+        color:#fff;
+    }
+    
+    #show:hover{
+        cursor:pointer;
+    }*/
+    
+    #win {
+        border:5px solid #0F1214;
+        position:fixed;
+        background-color:#14171A;
+        z-index:5;
+        top:15%;
+        left:15%;
+        right:15%;
+        box-shadow: rgba(0,0,0,.5) 0 0 0 1000px;
+        bottom:15%;
+    }
+    
+    #hide {
+        float:right;
+        background-color:transparent;
+        border:none;
+        color:#fff;
+        width:5%;
+        height:5%;
+    }
+    
+    #hide:hover{
+        cursor:pointer;
+        background-color:#FF0000;
+    }
+</style>
